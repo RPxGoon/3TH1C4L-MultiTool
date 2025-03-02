3e2fd5aa1874e5ed64257581d19247ca66c763ca
RPxGoon <114786172+RPxGoon@users.noreply.github.com>
2025-03-02 02:00:18 +0000

    [+] Multiple small fixes
    fixed false error messages, removed extra relay messages

--- setup.bat
+++ setup.bat
@@ -5,13 +5,11 @@ echo.
 echo [!] Checking for Python installation...
 echo.
 
-
 REM Check if Python is installed and available in PATH
 python --version >nul 2>&1
 if errorlevel 1 (
-    echo [!] Python is NOT INSTALLED! - Attempting to Install Python Silently...
+    echo [!] Python is NOT INSTALLED!
     echo.
-
 
     REM Check if winget is available
     where winget >nul 2>&1
@@ -22,16 +20,12 @@ if errorlevel 1 (
     )
 
     REM Use winget to install Python silently with the correct argument
-    echo [*] Installing Python via winget...
-    winget install --id Python.Python.3.11 --source winget --silent --accept-package-agreements --verbose-logs > install_log.txt 2>&1
+    echo [*] Installing Python via winget... Accept UAC popup in taskbar
+    winget install --id Python.Python.3.11 --source winget --silent --accept-package-agreements >nul 2>&1
 
     REM Wait for the installation process to complete (added delay)
     timeout /t 10 /nobreak >nul
 
-    REM Reopen the batch file to check installation after the installation process
-    echo [*] Reopening setup.bat to verify installation...
-    start "" "%~f0"
-    exit /b
 )
 
 echo [*] Python is Installed! Checking and Installing Required Packages...
@@ -40,7 +34,7 @@ echo [!] Upgrading pip...
 python -m ensurepip >nul 2>&1
 python -m pip install --upgrade pip >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [!] Failed to Upgrade 'pip'. Ensure Python is Properly Installed.
+    echo [!] Restart setup.bat to finish installation 
     pause
     exit /b 1
 )
@@ -58,5 +52,4 @@ echo [*] All Required Packages Installed Successfully! :)
 echo [*] Running the Main Tool (3th1c4l.py)...
 start "" python "%~dp0\3th1c4l.py"
 
-echo [*] Setup Complete! The Main Tool Will Now Open Automatically...
 pause
